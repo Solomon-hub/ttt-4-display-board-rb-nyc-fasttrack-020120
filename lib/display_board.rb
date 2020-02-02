ab57e9c14909e3061c70a3f10e56168cb7926f3b
Solomon Chu <solomonchu@live.com>
2020-02-02 20:23:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,39 +1,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-display_board(board)  
-
-board1 = [" "," "," "," ","X"," "," "," "," "]
-
-display_board(board1)
-
-board2 = ["O"," "," "," "," "," "," "," "," "]
-
-display_board(board2)
-
-board3 = ["O"," "," "," ","X"," "," "," "," "]
-
-display_board(board3)
-
-board4 = ["X","X","X"," "," "," "," "," "," "]
-
-display_board(board4)
-
-board5 = [" "," "," "," "," "," ","O","O","O"]
-
-display_board(board5)
-
-board6 = ["X"," "," "," ","X"," "," "," ","X"]
-
-display_board(board6)
-
-board7 = [" "," ","O"," ","O"," ","O"," "," "]
-
-display_board(board7)
 
