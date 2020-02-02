1dca8d287d7ad6eb748601fb79d4ba648f74cbb0
Solomon Chu <solomonchu@live.com>
2020-02-02 20:12:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,39 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board(board)  
+
+board1 = [" "," "," "," ","#{X}"," "," "," "," "]
+
+display_board(board1)
+
+board2 = ["O"," "," "," "," "," "," "," "," "]
+
+display_board(board2)
+
+board3 = ["O"," "," "," ","X"," "," "," "," "]
+
+display_board(board3)
+
+board4 = ["X","X","X"," "," "," "," "," "," "]
+
+display_board(board4)
+
+board5 = [" "," "," "," "," "," ","O","O","O"]
+
+display_board(board5)
+
+board6 = ["X"," "," "," ","X"," "," "," ","X"]
+
+display_board(board6)
+
+board7 = [" "," ","O"," ","O"," ","O"," "," "]
+
+display_board(board7)
+
